b0469606d2c8c7c98a1fcdfae7537947e83a7431
Papun Kumar Mohapatra <>
2021-05-01 21:21:49 +0530

    Chapter 2 : Return a Bean Object

--- src/main/java/com/pratice/mvc/controller/HelloWorldController.java
+++ src/main/java/com/pratice/mvc/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.pratice.mvc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pratice.mvc.model.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
 
@@ -13,6 +15,13 @@ public class HelloWorldController {
 	public String helloWorld() {
 		
 		return "Hi";
+		
+	}
+	
+	@GetMapping(path = "/hello-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Welcome");
 		
 	}
 }
---
+++ src/main/java/com/pratice/mvc/model/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.pratice.mvc.model;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
